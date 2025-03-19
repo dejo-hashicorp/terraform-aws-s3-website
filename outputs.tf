2940c55c0799f5dd44ef7b559ec8d52a4588d6b3
dejo-hashicorp <171065576+dejo-hashicorp@users.noreply.github.com>
2025-03-19 13:20:16 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,16 @@ output "website_endpoint" {
   description = "Website endpoint URL"
   value       = aws_s3_bucket_website_configuration.s3_bucket.website_endpoint
 }
+
+# Outputs
+
+output "access_key_id" {
+  description = "The Access Key ID generated for the IAM user to use with the storage bucket."
+  value       = aws_iam_access_key.bucket_access_key.id
+}
+
+output "secret_access_key" {
+  description = "The Secret Access Key generated for the IAM user to use with the storage bucket."
+  value       = aws_iam_access_key.bucket_access_key.secret
+  sensitive   = true
+}
